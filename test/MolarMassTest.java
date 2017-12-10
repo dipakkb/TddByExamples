c345fc028088a3cfd7e27d044d50737277333adf
dipak <dipak.h@gmail.com>
2017-12-10 18:47:49 +0530

    renamed tests

--- test/MolarMassTest.java
+++ test/MolarMassTest.java
@@ -6,31 +6,31 @@ import static org.junit.Assert.*;
  */
 public class MolarMassTest {
     @Test
-    public void test_molar_mass_of_carbon_is_12(){
+    public void testMolarMassOfCarbonIs12(){
         assertEquals(12, MolarMass.of("C"));
     }
 
     @Test
-    public void test_molar_mass_of_hydrogen_is_1(){
+    public void testMolarMassOfHydrogenIs1(){
         assertEquals(1, MolarMass.of("H"));
     }
 
     @Test
-    public void test_molar_mass_of_iron_is_26(){
+    public void testMolarMassOfIronIs26(){
         assertEquals(56, MolarMass.of("Fe"));
     }
 
     @Test
-    public void test_molar_mass_of_H2O_is_18() {
+    public void testMolarMassOfH2OIs18() {
         assertEquals(18, MolarMass.of("H2O"));
     }
     @Test
-    public void test_molar_mass_of_calcium_phospate_is_18() {
+    public void testMolarMassOfCalciumPhospateIs18() {
         assertEquals(310, MolarMass.of("Ca3(PO4)2"));
     }
 
     @Test
-    public void test_molar_mass_of_cobalt_ferricyanide_is_601() {
+    public void testMolarMassOfCobaltFerricyanideIs601() {
         assertEquals(601, MolarMass.of("Co3(Fe(CN)6)2"));
     }
 }
